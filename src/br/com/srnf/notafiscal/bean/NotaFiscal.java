27c55ba3cdc938c8cb3fea6d7d63c98478afa50a
Michael Santos Sim <michaelsantossim@outlook.com>
2016-03-13 00:08:51 -0300

    Implementação do Bean
    Inseridas as classes do Tomador, Nota e Endereço

---
+++ src/br/com/srnf/notafiscal/bean/NotaFiscal.java
@@ -0,0 +1,127 @@
+package br.com.srnf.notafiscal.bean;
+
+public class NotaFiscal {
+	
+	private Tomador tomador;
+	private boolean tomadorEstrangeiro;
+	private String informacoesAdicionais;
+	private String atividade;
+	private float valor;
+	private float valorIr;
+	private float valorInss;
+	private float valorCofins;
+	private float valorPisPasep;
+	private float Csll;
+	private float valorOutrosImpostos;
+	private boolean substituicaoTributaria;
+	private boolean notificarTomadorPorEmail;
+	private String enderecoPrestacaoServico;
+	private String cidadePrestacaoServico;
+	private String cepPrestacaoServico;
+	private String estadoPrestacaoServico;
+	
+	public Tomador getTomador() {
+		return tomador;
+	}
+	public void setTomador(Tomador tomador) {
+		this.tomador = tomador;
+	}
+	public boolean isTomadorEstrangeiro() {
+		return tomadorEstrangeiro;
+	}
+	public void setTomadorEstrangeiro(boolean tomadorEstrangeiro) {
+		this.tomadorEstrangeiro = tomadorEstrangeiro;
+	}
+	public String getInformacoesAdicionais() {
+		return informacoesAdicionais;
+	}
+	public void setInformacoesAdicionais(String informacoesAdicionais) {
+		this.informacoesAdicionais = informacoesAdicionais;
+	}
+	public String getAtividade() {
+		return atividade;
+	}
+	public void setAtividade(String atividade) {
+		this.atividade = atividade;
+	}
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	public float getValorIr() {
+		return valorIr;
+	}
+	public void setValorIr(float valorIr) {
+		this.valorIr = valorIr;
+	}
+	public float getValorInss() {
+		return valorInss;
+	}
+	public void setValorInss(float valorInss) {
+		this.valorInss = valorInss;
+	}
+	public float getValorCofins() {
+		return valorCofins;
+	}
+	public void setValorCofins(float valorCofins) {
+		this.valorCofins = valorCofins;
+	}
+	public float getValorPisPasep() {
+		return valorPisPasep;
+	}
+	public void setValorPisPasep(float valorPisPasep) {
+		this.valorPisPasep = valorPisPasep;
+	}
+	public float getCsll() {
+		return Csll;
+	}
+	public void setCsll(float csll) {
+		Csll = csll;
+	}
+	public float getValorOutrosImpostos() {
+		return valorOutrosImpostos;
+	}
+	public void setValorOutrosImpostos(float valorOutrosImpostos) {
+		this.valorOutrosImpostos = valorOutrosImpostos;
+	}
+	public boolean isSubstituicaoTributaria() {
+		return substituicaoTributaria;
+	}
+	public void setSubstituicaoTributaria(boolean substituicaoTributaria) {
+		this.substituicaoTributaria = substituicaoTributaria;
+	}
+	public boolean isNotificarTomadorPorEmail() {
+		return notificarTomadorPorEmail;
+	}
+	public void setNotificarTomadorPorEmail(boolean notificarTomadorPorEmail) {
+		this.notificarTomadorPorEmail = notificarTomadorPorEmail;
+	}
+	public String getEnderecoPrestacaoServico() {
+		return enderecoPrestacaoServico;
+	}
+	public void setEnderecoPrestacaoServico(String enderecoPrestacaoServico) {
+		this.enderecoPrestacaoServico = enderecoPrestacaoServico;
+	}
+	public String getCidadePrestacaoServico() {
+		return cidadePrestacaoServico;
+	}
+	public void setCidadePrestacaoServico(String cidadePrestacaoServico) {
+		this.cidadePrestacaoServico = cidadePrestacaoServico;
+	}
+	public String getCepPrestacaoServico() {
+		return cepPrestacaoServico;
+	}
+	public void setCepPrestacaoServico(String cepPrestacaoServico) {
+		this.cepPrestacaoServico = cepPrestacaoServico;
+	}
+	public String getEstadoPrestacaoServico() {
+		return estadoPrestacaoServico;
+	}
+	public void setEstadoPrestacaoServico(String estadoPrestacaoServico) {
+		this.estadoPrestacaoServico = estadoPrestacaoServico;
+	}
+	
+	
+}
---
+++ src/br/com/srnf/notafiscal/bean/Tomador.java
@@ -0,0 +1,58 @@
+package br.com.srnf.notafiscal.bean;
+
+public class Tomador {
+	
+	private String cnpj;
+	private String cpf;
+	private int ddd;
+	private String endereco;
+	private String inscricaoMunicipal;
+	private String nome;
+	private String telefone;
+	
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getDdd() {
+		return ddd;
+	}
+	public void setDdd(int ddd) {
+		this.ddd = ddd;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getInscricaoMunicipal() {
+		return inscricaoMunicipal;
+	}
+	public void setInscricaoMunicipal(String inscricaoMunicipal) {
+		this.inscricaoMunicipal = inscricaoMunicipal;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
